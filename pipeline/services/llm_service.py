07d410fc9d5627a011511ba4d792bac87ba8fd8d
Nguyen Duc Manh <167436213+manhnd52@users.noreply.github.com>
2026-06-19 00:25:09 +0700

    fix be 3

--- pipeline/services/llm_service.py
+++ pipeline/services/llm_service.py
@@ -2,6 +2,10 @@ from typing import Optional
 from langchain.chat_models import BaseChatModel, init_chat_model
 from langchain_openai import ChatOpenAI
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 model = init_chat_model("gpt-5-nano", temperature=0.0)
 
 inference_server_url = "http://liacara-dev-ai-service-alb-405568302.eu-central-1.elb.amazonaws.com/v1"
