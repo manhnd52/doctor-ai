65484dd8f12746584a023fe175b801f0b4d15b49
Nguyen Duc Manh <167436213+manhnd52@users.noreply.github.com>
2026-06-19 09:37:09 +0700

    fix frontend 02

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,8 +18,7 @@ export default defineConfig({
     }
   },
   server: {
-    port: 3000,
-    host: true,
+    allowedHosts: ['manhnd52.id.vn']
   },
   plugins: [react(), tailwindcss()],
   test: {
